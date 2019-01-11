513077a9d2e2a7bc0c4d3a6d38f9e400ad697723
peter <hofstedemail@yahoo.com>
2019-01-12 00:24:53 +0100

    insert and get steps

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
 
   <div id="app">
-    <li v-for="blueprint in blueprints">{{blueprint.name}}</li>
+    <li v-for="blueprint in blueprints" @click="getSteps(blueprint)" >{{blueprint.name}}</li>
     <input placeholder="add blueprint" @keyup.enter="addBlueprint($event)"/>
+    
+    
+    <h2>{{selected_blueprint.name}}</h2>
+
+    <li v-for="step in steps">{{step.name}}</li>
+    <input placeholder="add step" @keyup.enter="addStep($event)"/>
+
   </div>
 
-  
 
 </template>
 
@@ -18,22 +24,29 @@ export default {
   data: function() {
     return { 
       blueprints: [],
-      db: ""
+      db: "",
+      blueprints_ref: "",
+      steps_ref: "",
+      steps: [],
+      selected_blueprint: ""
     }
   },
   methods: {
 
     addBlueprint: function(el) {
-      console.log(el.target.value)
-
-      this.db.collection("blueprints").add({name:el.target.value})
-        .then(function(docRef) {
-          console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function(error) {
-          console.error("Error adding document: ", error);
-        })
-        el.target.value = ""
+      blueprints_ref.add({name:el.target.value})
+      el.target.value = ""
+    },
+    getSteps: function(blueprint) {
+      //TODO change into onSnapShot
+      console.log("getSteps" + blueprint.id)
+      this.selected_blueprint = blueprint
+      this.steps = []
+      this.steps_ref.where("blueprint","==",blueprint.id).get().then( snap => { snap.forEach( doc => {this.steps.push(doc.data())} )})
+    },
+    addStep: function(el) {
+     this.steps_ref.add({blueprint: this.selected_blueprint.id,name: el.target.value})
+     el.target.value = ""
     }
 
   },
@@ -41,7 +54,7 @@ export default {
 
     
     //Database setup
-
+    //TODO move to seperate file
     const config = {
         apiKey: "AIzaSyC58pRaWYD7TkA9cgXy9Bp17Njvi_5hm6c",
         authDomain: "ptool-01.firebaseapp.com",
@@ -61,15 +74,25 @@ export default {
         timestampsInSnapshots: true
       })
 
+      this.blueprints_ref = this.db.collection("blueprints")
+      this.steps_ref = this.db.collection("steps")
+
+
       //TODO check if this is now global and if so change
       let varscope = this
 
       //realtime triggered updates
       this.db.collection("blueprints").onSnapshot(function(snap){
         console.log("change detected")
-        
         varscope.blueprints = []
-        snap.forEach( doc => {varscope.blueprints.push(doc.data())})
+        snap.forEach( doc => {
+          
+          let obj = doc.data()
+          obj.id = doc.id
+
+          varscope.blueprints.push(obj)
+        
+        })
         
       })
 
@@ -99,6 +122,15 @@ export default {
 input {
   padding: 6px;
   margin-top: 6px;
+  display: block;
+}
+
+button {
+  background-color: white;
+  margin-top: 6px;
+  margin-bottom: 6px;
+  padding: 6px;
+  border-radius: 6px;
 }
 
 </style>
