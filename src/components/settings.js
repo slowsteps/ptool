4547f6de62eb0dd04117bf52f5df12c081ff507e
peter <hofstedemail@yahoo.com>
2019-01-13 21:47:28 +0100

    remove star trek tests

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,49 +1,26 @@
 import firebase from 'firebase'
 
 
-export default {
-	name: "enterprise",
+const config = {
+    apiKey: "AIzaSyC58pRaWYD7TkA9cgXy9Bp17Njvi_5hm6c",
+    authDomain: "ptool-01.firebaseapp.com",
+    databaseURL: "https://ptool-01.firebaseio.com",
+    projectId: "ptool-01",
+    storageBucket: "ptool-01.appspot.com",
+    messagingSenderId: "833427644098"
+  }
 
-	speed: 10000,
+firebase.initializeApp(config)
 
-
-	jump: function() {
-		console.log("jumping to lightspeed..")
-	}
-
-}
-
-
-
-    const config = {
-        apiKey: "AIzaSyC58pRaWYD7TkA9cgXy9Bp17Njvi_5hm6c",
-        authDomain: "ptool-01.firebaseapp.com",
-        databaseURL: "https://ptool-01.firebaseio.com",
-        projectId: "ptool-01",
-        storageBucket: "ptool-01.appspot.com",
-        messagingSenderId: "833427644098"
-      }
-
-      firebase.initializeApp(config)
-     
-      var db = firebase.firestore()
+var db = firebase.firestore()
 
 
-      db.settings({
-        timestampsInSnapshots: true
-      })
+db.settings({
+	timestampsInSnapshots: true
+})
 
-      var blueprints_ref = db.collection("blueprints")
-      var steps_ref = db.collection("steps")
-
-
+var blueprints_ref = db.collection("blueprints")
+var steps_ref = db.collection("steps")
 
 
-
-
-
-function orbit() { console.log("entering orbit")}
-
-var shipclass = "c-class"
-
-export {orbit,shipclass,blueprints_ref,steps_ref}
+export {blueprints_ref,steps_ref}
